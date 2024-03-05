8c923633bafba42b18cc3ce64d5350ecc8726939
Unknow Programmer <136961914+underdust@users.noreply.github.com>
2024-03-05 15:01:48 +0700

    Update schema.py

--- schema.py
+++ schema.py
@@ -12,3 +12,11 @@ class Sign_up(BaseModel):
     user_password : str
     tel_num : str
     email : str
+    
+class Review(BaseModel):
+    user_name : str
+    hotel_name : str
+    comment : str
+    rating : int
+    time : str
+    images : list
