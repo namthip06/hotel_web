0e599b6c0577cbdcdf83ac1a28297a10c07a0692
Unknow Programmer <136961914+underdust@users.noreply.github.com>
2024-03-05 17:20:41 +0700

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,25 +1,32 @@
 from pydantic import BaseModel
 
-class Pay(BaseModel):
-    name : str
-    hotel : str
-    room : str
-    check_in : str
-    check_out : str
+class Payment(BaseModel):
+    user_id : int
+    reservation_id : int
 
 class Sign_up(BaseModel):
     user_name : str
     user_password : str
     tel_num : str
     email : str
-    
+
 class Review(BaseModel):
     user_name : str
     hotel_name : str
     comment : str
     rating : int
     time : str
-    images : list
+
+# class EditRoomRequest(BaseModel):
+#     hotel_name: str
+#     room_detail: str
+#     new_price: float
+#     new_guests: int
+
+# class RoomDetails(BaseModel):
+#     room_type: str
+#     price: float
+#     max_guests: int
 
 class Hotel(BaseModel):
     name : str
